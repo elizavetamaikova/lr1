a6ee7431cc353209c1cf0291a695df6826c9b824
elizavetamaikova <70896324+elizavetamaikova@users.noreply.github.com>
2020-11-08 21:01:52 +0300

    Update Student.cpp

--- sources/Student.cpp
+++ sources/Student.cpp
@@ -48,11 +48,11 @@ void Student::get_name(const json& j, student_inf& st){
 }
 
 void Student::get_debt(const json& j, student_inf& st){
-  if (j.is_null()){
+  if (j.is_null()) {
     st.debt.push_back(nullptr);
-  }else if (j.is_string()){
+  } else if (j.is_string()) {
     st.debt.push_back(j.get<std::string>());
-  }else if (j.is_array()){
+  } else if (j.is_array()) {
     for (json::const_iterator it = j.cbegin(); it != j.cend(); ++it)
       st.debt.push_back(it->get<std::string>());
   }
