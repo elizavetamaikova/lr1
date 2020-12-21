d6478582e351628a100b9c35d4839c677564956f
Liza Maykova <elizavetabmstu@yandex.ru>
2020-12-22 00:08:34 +0300

    over

--- cmake-build-debug/_3rdParty/Hunter/toolchain/_builds/CMakeFiles/3.17.3/CompilerIdCXX/CMakeCXXCompilerId.cpp
+++ cmake-build-debug/_3rdParty/Hunter/toolchain/_builds/CMakeFiles/3.17.3/CompilerIdCXX/CMakeCXXCompilerId.cpp
@@ -576,8 +576,7 @@ char const info_version[] = {
 char const info_version_internal[] = {
   'I', 'N', 'F', 'O', ':',
   'c', 'o', 'm', 'p', 'i', 'l', 'e', 'r', '_', 'v', 'e', 'r', 's', 'i', 'o',
-    'n', '_',
-  'i','n','t','e','r','n','a','l','[',
+    'n', '_', 'i', 'n', 't', 'e', 'r', 'n', 'a', 'l', '[',
   COMPILER_VERSION_INTERNAL, ']', '\0'};
 #endif
 
